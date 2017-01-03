0c8ad8d35c84b8633413442fda556b00ba324852
Eric Ball <ericball1229@gmail.com>
2017-01-02 22:40:22 -0500

    Add reading of a user's bio

--- public/index.js
+++ public/index.js
@@ -6,6 +6,7 @@
     const btnLogOut = document.getElementById("log-out-button");
     const logTitle = document.getElementById("logTitle");
     const loginPane = document.getElementById("loginPane");
+    const bioDiv = document.getElementById("bioDiv");
     const txtDB = document.getElementById("bio");
 
     btnLogIn.addEventListener('click', e => {
@@ -31,27 +32,40 @@
     });
 
     btnLogOut.addEventListener('click', e => {
-       firebase.auth().signOut();
+        firebase.auth().signOut();
     });
+
+    var uid;
 
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if (firebaseUser) {
+            uid = firebaseUser.uid;
             console.log(firebaseUser);
             btnLogOut.classList.remove("hide");
             logTitle.innerHTML = "Welcome, " + firebaseUser.email;
             loginPane.classList.add("hide");
+            bioDiv.classList.remove("hide");
+            const dbRefObject = firebase.database().ref().child("users/" + uid + "/bio");
+            dbRefObject.on('value', snap => {
+                console.log(snap.val());
+                txtDB.innerHTML = snap.val();
+            });
+
         } else {
             console.log("not logged in");
             btnLogOut.classList.add("hide");
             logTitle.innerHTML = "Login";
             loginPane.classList.remove("hide");
+            bioDiv.classList.add("hide");
         }
     });
 
-    const dbRefObject = firebase.database().ref().child("bio");
-    dbRefObject.on('value', snap => {
-        console.log(snap.val());
-        txtDB.innerHTML = snap.val();
-    });
+
+
+
+
+
+
+
 
 })();
